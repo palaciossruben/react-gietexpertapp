30e2defe219aec6022fd9eebe20043c4413a760f
Ruben Palacios <palaciossruben@gmail.com>
2022-05-11 16:53:22 -0400

    first commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { Addcategory } from './components/Addcategory'
+import { GifGrid } from './components/GifGrid'
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['Venezuela'])
+
+  return (
+    <div>
+        <h2>GifExpertApp</h2>
+            <Addcategory setCategories={setCategories} />
+        <hr/>
+
+        <ol>
+            {
+                categories.map(category => 
+                    <GifGrid 
+                        key={category}
+                        category={category}
+                    />
+                )
+            }
+        </ol>
+    </div>
+  )
+}
